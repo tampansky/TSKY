27cc05227350f4888739c0f77c4b9c3a6c99bc8f
tampansky <42198256+tampansky@users.noreply.github.com>
2019-05-16 14:47:14 +0700

    Add files via upload

---
+++ tsky.sh
@@ -0,0 +1,369 @@
+#!/bin/bash
+#////////////////////////////////////////////////////////////////////
+#////////////////////////////////////////////////////////////////////
+#///          ________  ______   __    __  __      __             ///
+#///         /        |/      \ /  |  /  |/  \    /  |            ///
+#///         $$$$$$$$//$$$$$$  |$$ | /$$/ $$  \  /$$/             ///
+#///            $$ |  $$ \__$$/ $$ |/$$/   $$  \/$$/              ///
+#///            $$ |  $$      \ $$  $$<     $$  $$/               ///
+#///            $$ |   $$$$$$  |$$$$$  \     $$$$/                ///
+#///            $$ |  /  \__$$ |$$ |$$  \     $$ |                ///
+#///            $$ |  $$    $$/ $$ | $$  |    $$ |                ///
+#///            $$/    $$$$$$/  $$/   $$/     $$/                 ///
+#////////////////////////////////////////////////////////////////////
+#////////////////////////////////////////////////////////////////////                            
+#Peringatan!!!
+#Jangan Sampai Klean Menggunakan Ini Utk Kejahatan:)
+#colors
+blue='\e[1;34m'
+cyan='\e[0;36m'
+green='\e[0;34m'
+okegreen='\033[92m'
+lightgreen='\e[1;32m'
+grey="\033[0;37m"
+purple="\033[0;35m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+mon="mon"
+lightcyan='\e[96m'
+white='\e[1;37m'
+nc="\e[0m"
+red='\e[1;31m'
+yellow='\e[1;33m'
+
+sleep 0.25
+clear 
+echo -e "$red"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+sleep 0.25
+clear 
+echo -e "$blue"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+sleep 0.25
+clear 
+echo -e "$cyan"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+sleep 0.25
+clear 
+echo -e "$red"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+sleep 0.25
+clear 
+echo -e "$blue"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+sleep 0.25
+clear 
+echo -e "$cyan"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+sleep 0.25
+clear 
+echo -e "$red"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+sleep 0.25
+clear 
+echo -e "$blue"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+sleep 0.25
+clear 
+echo -e "$cyan"
+echo" _________________      ______   ____    ____  _____      _____ "
+echo"/                 \ ___|\     \ |    |  |    ||\    \    /    /|"
+echo"\______     ______/|    |\     \|    |  |    || \    \  /    / |"
+echo"   \( /    /  )/   |    |/____/||    | /    //|  \____\/    /  /"
+echo"    ' |   |   ' ___|    \|   | ||    |/ _ _//  \ |    /    /  / "
+echo"      |   |    |    \    \___|/ |    |\    \'   \|___/    /  /  "
+echo"     /   //    |    |\     \    |    | \    \       /    /  /   "
+echo"    /___//     |\ ___\|_____|   |____|  \____\     /____/  /    "
+echo"   |`   |      | |    |     |   |    |   |    |   |`    | /     "
+echo"   |____|       \|____|_____|   |____|   |____|   |_____|/      "
+echo"     \(            \(    )/       \(       )/        )/         "
+echo"      '             '    '         '       '         '          "
+echo -e "$nc"
+ 
+ 
+tsky(){
+clear
+sleep 0.03
+echo"   /$$$$$$$$ /$$$$$$  /$$   /$$ /$$     /$$"
+sleep 0.03
+echo"  |__  $$__//$$__  $$| $$  /$$/|  $$   /$$/"
+sleep 0.02
+echo"     | $$  | $$  \__/| $$ /$$/  \  $$ /$$/ "
+sleep 0.03
+echo"     | $$  |  $$$$$$ | $$$$$/    \  $$$$/  "
+sleep 0.03
+echo"     | $$   \____  $$| $$  $$     \  $$/   "
+sleep 0.02
+echo"     | $$   /$$  \ $$| $$\  $$     | $$    "
+sleep 0.03
+echo"     | $$  |  $$$$$$/| $$ \  $$    | $$    "
+sleep 0.03
+echo"     |__/   \______/ |__/  \__/    |__/    "
+sleep 0.02                                         
+}                                                       	
+load(){
+    echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+    done
+}
+}
+get_sms=$(curl -s http://zlucifer.com/api/sms.php)
+get_call=$(curl -s http://zlucifer.com/api/call.php)
+mulai (){
+    echo "Gunakan TSKY?"
+    echo "y/n?"
+    echo
+    read lagi
+    if [ $lagi = "y" ]; then
+            spam
+    else
+            echo "Silahkan Gunakan Sesuai Dengan Kebutuhan"
+    fi
+}
+spam(){
+    clear
+    troll
+    echo -e $red"============"
+    echo -e $blue"(1.)Tsky SMS."
+    echo -e $red"============"
+    ehco -e $Purple"(2.)Tsky Telepon."
+    echo -e $red"============"
+    echo -e $cyan"(3.)EXIT."
+    echo -e $red"============"
+    echo "1/2/3?"
+    echo -e $red"============="
+    echo -e $blue"╭─sky"
+    read -p "╰─#" pilih
+
+   if [ $pilih = "1" ]; then
+   	        echo -e $red"============="
+            echo -e $blue"TSKY Spam SMS"
+            echo -e $red"============="
+            #function spam
+            echo -e $yellow"Silahkan masukan nomor telp target"
+            echo contoh 0812345678
+            echo -e $blue"╭─sky"
+            read -p "╰─#" pilih
+            echo
+            echo -e $green"Berapa sms yang mau dikirim?"
+            read paket
+            echo
+            echo Apakah nomor $target "dan SMS dikirim "$paket" sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                    load
+                    clear
+                    echo Melakukan spam SMS ke nomor $target 
+                    echo "Jangan close aplikasi sebelum spam selesai"
+                    echo "========================================"
+                    target_do=$get_sms'/sms.php?nomor='$target'&paket='$paket
+                    CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
+                    echo " Gunakan tools dengan bijak"
+                    echo
+                    echo " Tools SpamSMS "
+                    echo " TampanSKY"
+                    echo "======================================="
+            else
+                    echo "Kesalahan"
+
+             fi
+            mulai
+            elif [ $pilih = "2" ]; then
+   	        echo -e $red"============="            	
+            echo -e $purple"TSKY Spam Telepon"
+   	        echo -e $red"============="            
+            #function spam
+            echo
+            echo -e $yellow"Silahkan masukan nomor telp target"
+            echo contoh 0812345678
+            read target # masukin no telp
+            echo -e $red"============="
+            echo -e $blue"Gunakan API Grab/Toped?"
+            echo -e $red"============="
+            echo -e $purple"[1] GRAB"
+            echo -e $red"============="
+            echo -e $cyan"[2] TOPED"
+            echo -e $red"============="
+            echo -e $purple"[3] LAZADA"
+            echo -e $red"============="
+            echo -e $cyan"[4] BUKALAPAK"
+            echo -e $red"============="
+            echo -e $purple"[5] GOJEK"
+            echo -e $red"============="
+            echo "1/2/3/4/5?"
+            echo -e $red"============="
+            echo -e $blue"╭─sky"
+            read -p "╰─#" pilih
+            if [ $api = "1" ]; then
+                  api_spam="grab"
+            else
+                  api_spam="toped"
+            else 
+            	  api_spam="lazada"
+            else 
+            	  api_spam="bukalapak"
+            else 
+            	  api_spam="gojek"
+            fi
+            echo Apakah nomor $target dan spam menggunakan $api_spam "sudah benar?"
+            echo y/n?
+            echo -e $blue"╭─sky"
+            read -p "╰─#" pilih
+            echo
+            if [ $confirm = "y" ]; then
+                  load
+                  clear
+                  echo Melakukan spam call ke nomor $target
+                  echo
+                  echo -e $blue"Jangan close aplikasi sebelum spam selesai"
+                  echo -e $red"========================================"
+                  cek_target=`curl -s $get_call/call.php?nomor=$target"&method="$api_spam`
+                  echo -e $cek_target
+                  echo -e blue" Gunakan tools dengan bijak"
+                  echo
+                  echo -e $purple" TOOLS SPAM TELEPON "
+                  echo -e $cyan" TampanSky"
+                  echo -e $red"========================================"
+            else
+                  echo Kesalahan, silahkan coba lagi
+            fi
+        mulai
+        elif [ $pilih = "3" ]; then
+        echo -e $blue"Terimakasih Telah Menggunakan TSKY"
+        close
+    else
+        clear
+        echo -e $red"Kesalahan"
+        mulai
+            fi
+
+}
+close(){
+    exit
+}
+clear
+echo "Loading.."
+load
+clear
+troll
+echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
+read nick #membaca yang ditulis
+clear
+troll
+echo Selamat datang $nick ":)"
+echo
+echo "Mulai TSKY?"
+echo "y/n?"
+read mulai
+if [ $mulai = "y" ]; then
+      spam
+else
+      close
+fi
